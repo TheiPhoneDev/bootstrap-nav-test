53c26baf06d3edae1ce7deef315facb1fb33077e
TheiPhoneDev <73944807+TheiPhoneDev@users.noreply.github.com>
2024-02-18 17:13:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function testButton1() {
+  console.log("Test ciclo while, print numeri da 1 a 100");
+  let i=1;
 
+  while(i<=100) {
+    console.log(i);
+    i++;
+  }
+
+  console.log("-------------------------------------------");
+
+}
