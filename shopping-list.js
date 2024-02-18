a7eb88c3f0a34fa602e6a6682d9c82064e00282d
TheiPhoneDev <73944807+TheiPhoneDev@users.noreply.github.com>
2024-02-18 17:14:00 +0100

    Update shopping-list.js

--- shopping-list.js
+++ shopping-list.js
@@ -1,1 +1,25 @@
+const shoppingList = document.getElementById("shoppingList");
 
+const aggiuntaElemento = document.getElementById("aggiuntaElemento");
+
+aggiuntaElemento.addEventListener("keydown", function(e) {
+  if(e.key === "Enter" && e.target.value.trim()) {
+    creaElemento(e.target.value)
+    e.target.value = "";
+  }
+})
+
+
+shoppingList.addEventListener("click", (e) => {
+  if(e.target.classList.contains("cestino")) {
+    e.target.parentElement.remove();
+  }
+})
+
+
+const creaElemento = (element) => {
+  const listElement = document.createElement("li")
+  listElement.className = "list-group-item d-flex justify-content-between";
+  listElement.innerHTML = `<span>${element}</span><i class="bi bi-trash3 cestino"></i>`;
+  shoppingList.appendChild(listElement);
+}
